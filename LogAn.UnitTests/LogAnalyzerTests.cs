439759df97e3ac5e19f5ff00972bf8f940a8b007
oozturkplus@gmail.com <oozturkplus@gmail.com>
2017-01-26 00:44:44 +0300

    first commit

---
+++ LogAn.UnitTests/LogAnalyzerTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using UntitTestWork;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        [Test]
+        public void IsValidFileName_BadExtension_ReturnsFalse()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            bool result = analyzer.IsValidLogFileName("fgsdf.xxx");
+            Assert.False(result);
+        }
+
+        [TestCase("filewithgoodextension.SLF")]
+        [TestCase("filewithgoodextension.slf")]
+        public void IsValidLogFileName_ValidExtensions_ReturnsTrue(string file)
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            bool result = analyzer.IsValidLogFileName(file);
+            Assert.True(result);
+        }
+
+        [Test]
+        public void IsValidLogFileName_GoodExtensionUppercase_ReturnsTrue()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            bool result =
+            analyzer
+            .IsValidLogFileName("filewithgoodextension.SLF");
+            Assert.True(result);
+        }
+
+        [TestCase("filewithgoodextension.SLF", true)]
+        [TestCase("filewithgoodextension.slf", true)]
+        [TestCase("filewithbadextension.foo", false)]
+        public void IsValidLogFileName_VariousExtensions_ChecksThem(string file,bool expected)
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            bool result = analyzer.IsValidLogFileName(file);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ UntitTestWork/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UntitTestWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public bool IsValidLogFileName(string fileName)
+        {
+            if (fileName.EndsWith(".SLF"))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
